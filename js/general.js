50f52e074b704d7e626d318299251727c9725a0a
finspin <jarospisak@gmail.com>
2018-01-15 16:35:40 +0200

    Fixed table sorting

--- js/general.js
+++ js/general.js
@@ -3,4 +3,9 @@ var options = {
     valueNames: [ { name: 'name', attr: 'data-name' }, 'city', 'country', { name: 'date', attr: 'data-start-date' }, 'cost' ]
 };
 
+var mobileOptions = {
+    valueNames: [ { name: 'name', attr: 'data-name' }, { name: 'date', attr: 'data-start-date' }, 'cost' ]
+};
+
 var conferenceList = new List('conferences', options);
+var conferenceListMobile = new List('conferences-mobile', mobileOptions);
