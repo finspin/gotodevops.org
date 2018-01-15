5a8e3a2e5a3a3fbbc367841d3add71f43cc87507
finspin <jarospisak@gmail.com>
2018-01-15 17:18:38 +0200

    Revert "Fixed table sorting"
    This reverts commit 50f52e074b704d7e626d318299251727c9725a0a.

--- js/general.js
+++ js/general.js
@@ -3,9 +3,4 @@ var options = {
     valueNames: [ { name: 'name', attr: 'data-name' }, 'city', 'country', { name: 'date', attr: 'data-start-date' }, 'cost' ]
 };
 
-var mobileOptions = {
-    valueNames: [ { name: 'name', attr: 'data-name' }, { name: 'date', attr: 'data-start-date' }, 'cost' ]
-};
-
 var conferenceList = new List('conferences', options);
-var conferenceListMobile = new List('conferences-mobile', mobileOptions);
