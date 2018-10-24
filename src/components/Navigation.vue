d50d1f83d8817d14b4b54528528fbffed49fac8a
Jaro Spisak <jarospisak@gmail.com>
2018-10-24 20:14:02 +0300

    Highlight subscribe link in navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -18,7 +18,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item d-none d-md-block">
-            <a class="nav-link" href="https://www.subscribepage.com/devops-conferences" rel="nofollow"><i class="fas fa-envelope"></i> SUBSCRIBE</a>
+            <a class="nav-link subscribe" href="https://www.subscribepage.com/devops-conferences" rel="nofollow"><i class="fas fa-envelope"></i> SUBSCRIBE</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="https://github.com/finspin/gotodevops.org#how-can-i-contribute" rel="nofollow"><i class="fas fa-plus"></i> ADD CONFERENCE</a>
@@ -76,6 +76,11 @@
 
   ul a {
     color: #999;
+  }
+  
+  ul a.subscribe {
+    color: #ff4954;
+    font-weight: bold;
   }
 
   .navbar-toggler-icon {
