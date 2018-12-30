bff1198460dc51ecdeb56d46c6734f675b2bdcf8
finspin <jarospisak@gmail.com>
2018-12-30 19:55:34 +0200

    Load bootstrap before app so that the styles are overriden

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.vue'
+import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUmbrellaBeach, faCloud, faSnowflake, faFilter, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faCalendarAlt, faQuestionCircle } from '@fortawesome/free-regular-svg-icons'
