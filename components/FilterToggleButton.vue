c328889648b67f6fc650d0d7af599a604034f102
finspin <jarospisak@gmail.com>
2019-02-02 12:51:05 +0200

    Remove filter button to try to fix issue with appendChild

--- components/FilterToggleButton.vue
+++ components/FilterToggleButton.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="d-inline-block">
-    <button class="btn btn-filter btn-link pr-0" v-b-toggle="'collapse-filters'" type="button">
-      <font-awesome-icon icon="filter" class="mr-1"/>
-      FILTER
-    </button>
+    <!--<button class="btn btn-filter btn-link pr-0" v-b-toggle="'collapse-filters'" type="button">-->
+      <!--<font-awesome-icon icon="filter" class="mr-1"/>-->
+      <!--FILTER-->
+    <!--</button>-->
   </div>
 </template>
 
