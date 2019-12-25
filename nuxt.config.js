82e2a05308e9bb3dfde38e648864a238d3fa08ad
Jaro Spisak <jarospisak@gmail.com>
2019-12-25 22:09:22 +0200

    Remove year 2019

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,49 +7,56 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: "List Of DevOps Conferences In 2019 / 2020",
+    title: "List Of DevOps Conferences In 2020",
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Nunito:400,800|Lato|Roboto&display=swap' }
-
+      { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css?family=Nunito:400,800|Lato|Roboto&display=swap"
+      }
     ],
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { name: "google-site-verification", content: "-vXHtXbcyvgpc0ozZbgAgosriGOjsII3rolId9kZeCo" },
-      { hid: "description", name: "description", content: "Find your next DevOps conference in 2019 from the most comprehensive list, maintained by a community of DevOps professionals. DevOps Conferences in USA, Europe, Asia and Australia &raquo;" }
+      {
+        name: "google-site-verification",
+        content: "-vXHtXbcyvgpc0ozZbgAgosriGOjsII3rolId9kZeCo"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Find your next DevOps conference in 2020 from the most comprehensive list, maintained by a community of DevOps professionals. DevOps Conferences in USA, Europe, Asia and Australia &raquo;"
+      }
     ]
   },
 
   /*
   ** Global CSS
   */
-  css: [
-    "@fortawesome/fontawesome-svg-core/styles.css",
-    "~/css/global.css"
-  ],
-
+  css: ["@fortawesome/fontawesome-svg-core/styles.css", "~/css/global.css"],
 
   /*
   ** Nuxt.js modules
   */
   modules: [
-    ['@nuxtjs/google-analytics', {
-      id: 'UA-60153-41'
-    }],
+    [
+      "@nuxtjs/google-analytics",
+      {
+        id: "UA-60153-41"
+      }
+    ],
     // Doc: https://bootstrap-vue.js.org/docs/
     "bootstrap-vue/nuxt"
   ],
 
-  plugins: [
-    { src: "~/plugins/font-awesome" }
-  ],
+  plugins: [{ src: "~/plugins/font-awesome" }],
 
   /*
   ** Build configuration
   */
   build: {
-
     // analyze: {
     //   analyzerMode: 'static'
     // },
@@ -58,7 +65,7 @@ module.exports = {
     */
     plugins: [
       new webpack.ProvidePlugin({
-        "_": "lodash"
+        _: "lodash"
       })
     ]
   }
