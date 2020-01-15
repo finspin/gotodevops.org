4307c3322c09e5ef06911e0ed9df7f80c34018b5
Jaro Spisak <jarospisak@gmail.com>
2020-01-15 21:27:18 +0200

    Add support for sponsored conferences

--- components/Conferences.vue
+++ components/Conferences.vue
@@ -1,39 +1,58 @@
 <template>
-    <transition-group id="conferences" class="container" name="fade" tag="div">
-      <conference v-for="conference in filteredConferences"
-                  :key="conference.url + conference.location.city"
-                  :conference=conference></conference>
+  <div>
+    <div class="container sponsored">
+      <conference
+        v-for="conference in sponsoredConferences"
+        :key="conference.url + conference.location.city"
+        :conference=conference
+      ></conference>
+    </div>
+    <transition-group
+      id="conferences"
+      class="container"
+      name="fade"
+      tag="div"
+    >
+      <conference
+        v-for="conference in filteredConferences"
+        :key="conference.url + conference.location.city"
+        :conference=conference
+      ></conference>
     </transition-group>
+  </div>
 </template>
 
 <script>
-  import Conference from './Conference'
-  import {mapGetters} from 'vuex'
+import Conference from "./Conference";
+import { mapGetters } from "vuex";
 
-  export default {
-    name: "Conferences",
-    components: {
-      Conference
-    },
-    computed: {
-      ...mapGetters([
-        'filteredConferences'
-      ])
-    }
+export default {
+  name: "Conferences",
+  components: {
+    Conference
+  },
+  computed: {
+    ...mapGetters(["filteredConferences", "sponsoredConferences"])
   }
+};
 </script>
 
 <style scoped>
-  #conferences {
-    padding-bottom: 10px;
-  }
+#conferences {
+  padding-bottom: 10px;
+}
 
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity .3s;
-  }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
 
-  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
-  {
-    opacity: 0;
-  }
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
+ {
+  opacity: 0;
+}
+
+.sponsored {
+  background-color: #fffbdb;
+}
 </style>
--- store/index.js
+++ store/index.js
@@ -1,6 +1,5 @@
-import Vuex from 'vuex'
-import conferences from '../static/conferences.json'
-
+import Vuex from "vuex";
+import conferences from "../static/conferences.json";
 
 const createStore = () => {
   return new Vuex.Store({
@@ -16,101 +15,149 @@ const createStore = () => {
     getters: {
       numberOfConferences: state => {
         // Don't count conferences with start day older than today
-        return _.filter(state.conferences, function (conference) {
-          const confStartDate = new Date(conference.date.start)
+        return _.filter(state.conferences, function(conference) {
+          const confStartDate = new Date(conference.date.start);
           if (confStartDate >= new Date()) {
-            return conference
+            return conference;
           }
-        }).length
+        }).length;
       },
       numberOfDisplayedConferences: (state, context) => {
-        return context.filteredConferences.length
+        return context.filteredConferences.length;
       },
-      filteredConferences: (state) => {
-        const activeCostFilter = state.filters.cost
-        const activeMonthFilter = state.filters.months
-        const activeWeatherFilter = state.filters.weather
-        const activeContinentFilter = state.filters.continent
+      sponsoredConferences: state => {
+        return _.filter(state.conferences, function(conference) {
+          return conference.sponsored;
+        });
+      },
+      filteredConferences: state => {
+        const activeCostFilter = state.filters.cost;
+        const activeMonthFilter = state.filters.months;
+        const activeWeatherFilter = state.filters.weather;
+        const activeContinentFilter = state.filters.continent;
 
-
-        let filteredConferences = _.filter(state.conferences, function (conference) {
-          let monthMatch, costMatch, continentMatch = false, weatherMatch = false, show = true
+        let filteredConferences = _.filter(state.conferences, function(
+          conference
+        ) {
+          let monthMatch,
+            costMatch,
+            continentMatch = false,
+            weatherMatch = false,
+            show = true;
 
           // Month filter
-          const locale = 'en-us'
-          const confStartDate = new Date(conference.date.start)
-          const confStartMonth = confStartDate.toLocaleString(locale, { month: 'short' }).toUpperCase()
-          const confStartYear = confStartDate.getFullYear().toString()
+          const locale = "en-us";
+          const confStartDate = new Date(conference.date.start);
+          const confStartMonth = confStartDate
+            .toLocaleString(locale, { month: "short" })
+            .toUpperCase();
+          const confStartYear = confStartDate.getFullYear().toString();
 
-          monthMatch = activeMonthFilter.length === 0 ? true : _.findIndex(state.filters.months, {
-            month: confStartMonth,
-            year: confStartYear
-          }) > -1
+          monthMatch =
+            activeMonthFilter.length === 0
+              ? true
+              : _.findIndex(state.filters.months, {
+                  month: confStartMonth,
+                  year: confStartYear
+                }) > -1;
 
           // Cost filter
-          costMatch = activeCostFilter === null ? true : conference.cost <= activeCostFilter
+          costMatch =
+            activeCostFilter === null
+              ? true
+              : conference.cost <= activeCostFilter;
 
           // Weather filter
           if (activeWeatherFilter === null) {
-            weatherMatch = true
-          } else if (activeWeatherFilter === 'cold' && conference.temperature.celsius < 10) {
-            weatherMatch = true
-          } else if (activeWeatherFilter === 'mild' && (conference.temperature.celsius >= 10 && conference.temperature.celsius < 20)) {
-            weatherMatch = true
-          } else if (activeWeatherFilter === 'warm' && conference.temperature.celsius >= 20) {
-            weatherMatch = true
+            weatherMatch = true;
+          } else if (
+            activeWeatherFilter === "cold" &&
+            conference.temperature.celsius < 10
+          ) {
+            weatherMatch = true;
+          } else if (
+            activeWeatherFilter === "mild" &&
+            (conference.temperature.celsius >= 10 &&
+              conference.temperature.celsius < 20)
+          ) {
+            weatherMatch = true;
+          } else if (
+            activeWeatherFilter === "warm" &&
+            conference.temperature.celsius >= 20
+          ) {
+            weatherMatch = true;
           }
 
           // Continent filter
           if (activeContinentFilter === null) {
-            continentMatch = true
-          } else if (activeContinentFilter === 'Americas' && ['North America', 'South America'].indexOf(conference.location.continent) > -1) {
-            continentMatch = true
-          } else if (activeContinentFilter === 'Europe' && conference.location.continent === 'Europe') {
-            continentMatch = true
+            continentMatch = true;
           } else if (
-            activeContinentFilter === 'Asia / Australia' &&
-            ['Asia', 'Australia'].indexOf(conference.location.continent) > -1
+            activeContinentFilter === "Americas" &&
+            ["North America", "South America"].indexOf(
+              conference.location.continent
+            ) > -1
           ) {
-            continentMatch = true
+            continentMatch = true;
+          } else if (
+            activeContinentFilter === "Europe" &&
+            conference.location.continent === "Europe"
+          ) {
+            continentMatch = true;
+          } else if (
+            activeContinentFilter === "Asia / Australia" &&
+            ["Asia", "Australia"].indexOf(conference.location.continent) > -1
+          ) {
+            continentMatch = true;
           }
 
           // Hide conferences with start day older than today
           if (confStartDate < new Date()) {
-            show = false
+            show = false;
           }
 
-          return monthMatch && costMatch && weatherMatch && continentMatch && show
-        })
+          return (
+            monthMatch &&
+            costMatch &&
+            weatherMatch &&
+            continentMatch &&
+            show &&
+            !conference.sponsored
+          );
+        });
 
-        return _.orderBy(filteredConferences, 'date.start')
+        return _.orderBy(filteredConferences, "date.start");
       }
     },
     mutations: {
       UPDATE_COST_FILTER: (state, cost) => {
-        state.filters.cost = state.filters.cost === cost ? null : cost
+        state.filters.cost = state.filters.cost === cost ? null : cost;
       },
       UPDATE_MONTH_FILTER: (state, payload) => {
-        const index = _.findIndex(state.filters.months, payload)
+        const index = _.findIndex(state.filters.months, payload);
 
-        index > -1 ? state.filters.months.splice(index, 1) : state.filters.months.push(payload)
+        index > -1
+          ? state.filters.months.splice(index, 1)
+          : state.filters.months.push(payload);
       },
       UPDATE_WEATHER_FILTER: (state, weather) => {
-        state.filters.weather = state.filters.weather === weather ? null : state.filters.weather = weather
+        state.filters.weather =
+          state.filters.weather === weather
+            ? null
+            : (state.filters.weather = weather);
       },
       UPDATE_CONTINENT_FILTER: (state, continent) => {
-        state.filters.continent = state.filters.continent === continent ? null : continent
+        state.filters.continent =
+          state.filters.continent === continent ? null : continent;
       },
       CLEAR_ALL_FILTERS: state => {
-        state.filters.cost = null
-        state.filters.months = []
-        state.filters.weather = null
-        state.filters.continent = null
+        state.filters.cost = null;
+        state.filters.months = [];
+        state.filters.weather = null;
+        state.filters.continent = null;
       }
     },
     actions: {}
+  });
+};
 
-  })
-}
-
-export default createStore
+export default createStore;
