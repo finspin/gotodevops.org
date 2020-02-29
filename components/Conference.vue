8f9c26ae154039af1b5b6eb9813e336e413df00e
Jaro Spisak <jarospisak@gmail.com>
2020-02-29 21:37:03 +0200

    Revert "Add outbound link tracking"

--- components/Conference.vue
+++ components/Conference.vue
@@ -2,7 +2,6 @@
   <a
     :href="conference.url"
     :rel="conference.sponsored && 'sponsored'"
-    @click="captureOutboundLink(conference.url); return false;"
   >
     <div class="conference row">
       <div class="col-12">
